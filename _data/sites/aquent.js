53a14223e81ef0d1a896ab7fcb85365eb0e7c58f
Michael Fienen <fienen@gmail.com>
2021-04-05 12:08:42 -0500

    Add my site config

---
+++ _data/sites/aquent.js
@@ -0,0 +1,22 @@
+module.exports = {
+	name: "Aquent Sites", // optional, falls back to object key
+	description: "Aquent's main websites",
+	options: {
+		frequency: 60 * 23, // 23 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "site"
+	},
+	urls: [
+		"https://aquent.com/",
+		"https://aquent.co.uk",
+		"https://aquent.com.au",
+		"https://aquent.nl",
+		"https://aquent.fr",
+		"https://aquent.de",
+		"https://aquent.co.jp",
+		"hhtps://aquentcareers.com"
+	]
+};
---
+++ _data/sites/vitamintalent.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: "Vitamin T Sites", // optional, falls back to object key
+	description: "Vitamin T's main websites",
+	options: {
+		frequency: 60 * 23, // 23 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "site"
+	},
+	urls: [
+		"https://vitamintalent.com/",
+    "https://checkmysalary.careers"
+	]
+};
